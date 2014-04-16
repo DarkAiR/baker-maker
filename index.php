3d5388c0b286bc5b4a9fc63485a5aeda0f27d015
DarkAiR <darkair@list.ru>
2014-04-17 02:53:28 +0600

    rus-eng version

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,7 +15,7 @@
     <link href="bootstrap-3.1.1/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="starter-template.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -25,44 +25,81 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
 
-  <body>
+    <script src="js/jquery-2.1.0.min.js"></script>
+    <script src="bootstrap-3.1.1/js/bootstrap.min.js"></script>
 
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <!--div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Baker-maker.ru</a>
+    <script type="text/javascript">
+        $(document).ready( function()
+        {
+            var swRus = $('#sw-rus');
+            var swEng = $('#sw-eng');
+            var logo = $('.logo');
+            var prevSw = false;
+            swRus.click( function()
+            {
+                if (prevSw)
+                    prevSw.toggleClass('active');
+                prevSw = swRus;
+                prevSw.toggleClass('active');
+                $('.info .eng').hide();
+                $('.info .rus').show();
+                logo.removeClass('eng').addClass('rus');
+            });
+            swEng.click( function()
+            {
+                if (prevSw)
+                    prevSw.toggleClass('active');
+                prevSw = swEng;
+                prevSw.toggleClass('active');
+                $('.info .rus').hide();
+                $('.info .eng').show();
+                logo.removeClass('rus').addClass('eng');
+            });
+            swRus.trigger('click');
+        });
+    </script>
+</head>
+
+<body>
+    <div class='container outer-cont'>
+        <div class='inner-cont content'>
+            <div class='logo'></div>
+            <div class='switcher'>
+                <div id='sw-rus'>RUS</div>
+                <div id='sw-eng'>ENG</div>
+            </div>
+            <div class='info'>
+                <div class='rus' style='display:none;'>
+                    <p>
+                        Спасибо, что заглянули к нам в мастерскую!<br>
+                        <i>Сейчас наш сайт находится в разработке!</i><br>
+                        Следите за нами на <a href="http://www.facebook.com/bakermakermoscow" target="_blank">Facebook</a> и <a href="http://instagram.com/baker_maker/" target="_blank">Instagram</a>
+                    </p>
+                    <p>
+                        Звоните и заказывайте наши сладости по тел.: <b>+7 926 625 75 90</b><br>
+                        Пишите нам письма на <a href="mailto:info@baker-maker.ru">info@baker-maker.ru</a>
+                    </p>
+                    <p>
+                        Портфолио с нашими работами вы можете посмотреть <a href="http://www.facebook.com/bakermakermoscow/photos_stream" target="_blank">тут</a>
+                    </p>
+                </div>
+                <div class='eng' style='display:none;'>
+                    <p>
+                        Thank you for your visit!<br>
+                        <i>Our page is under construction and it's coming soon!</i><br>
+                        Follow us on <a href="http://www.facebook.com/bakermakermoscow" target="_blank">Facebook</a> and <a href="http://instagram.com/baker_maker/" target="_blank">Instagram</a>
+                    </p>
+                    <p>
+                        Fancy to try our cakes, call us <b>+7 926 625 75 90</b><br>
+                        or e-mail us <a href="mailto:info@baker-maker.ru">info@baker-maker.ru</a>
+                    </p>
+                    <p>
+                        Want to see them first - <a href="http://www.facebook.com/bakermakermoscow/photos_stream" target="_blank">click here</a>
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-          </ul>
-        </div--><!--/.nav-collapse -->
-      </div>
     </div>
-
-    <div class="container">
-
-      <div class="starter-template">
-        <h1>Baker-maker.ru</h1>
-        <p class="lead">Site under re-construction</p>
-      </div>
-
-    </div><!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-  </body>
+</body>
 </html>
