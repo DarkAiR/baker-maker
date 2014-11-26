9411b3522fe787a6f546046105da36ec37c54c42
DarkAiR <darkair@list.ru>
2014-11-27 03:51:15 +0500

    menu, resize, mobile

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- link rel="shortcut icon" href="assets/ico/favicon.ico" -->
@@ -32,63 +31,79 @@
     <script type="text/javascript">
         $(document).ready( function()
         {
-            var swRus = $('#sw-rus');
-            var swEng = $('#sw-eng');
-            var logo = $('.logo');
-            var prevSw = false;
-            swRus.click( function()
-            {
-                if (prevSw)
-                    prevSw.toggleClass('active');
-                prevSw = swRus;
-                prevSw.toggleClass('active');
-                $('.info .eng').hide();
-                $('.info .rus').show();
-                logo.removeClass('eng').addClass('rus');
+            $(document).scroll(function() {
+                var scrTop = $(document).scrollTop();
+                var nav = $('#js-navigation');
+                var sections = $('[id^="js-sect"]');
+                var currIndex = 0;
+                sections.each( function(index) {
+                    if ($(this).offset().top - scrTop > 32)
+                        return false;
+                    currIndex = index;
+                });
+                // Start at 1
+                currIndex++;
+
+                nav.find('ul li').removeClass('active');
+                nav.find('ul li[data-sect='+currIndex+']').addClass('active');
             });
-            swEng.click( function()
-            {
-                if (prevSw)
-                    prevSw.toggleClass('active');
-                prevSw = swEng;
-                prevSw.toggleClass('active');
-                $('.info .rus').hide();
-                $('.info .eng').show();
-                logo.removeClass('rus').addClass('eng');
-            });
-            swRus.trigger('click');
+            $(document).trigger('scroll');
         });
     </script>
 </head>
 
 <body>
-    <div class='container-fluid'>
-            <div class='row  block1'>
-                <ul class='menu'>
-                    <li><a href='#contacts'>Контакты</a></li>
-                </ul>
-            </div>
-            <div class='row  block2'></div>
-            <div class='row  block3'></div>
-            <div class='row  block4'></div>
-            <div class='row  block5'></div>
-            <div class='row  block6'></div>
-            <div class='row  block7'></div>
-            <div class='row  block8'></div>
-            <div class='row  block9'></div>
-            <div class='row  block10'></div>
-            <div class='row  block11'></div>
-            <div class='row  block12'></div>
+    <div class='main-container  cont'>
+<!--
+        <div class='b'>
+            <img src='img/bg_01.jpg'>
+            <p>This is test text</p>
+        </div>
+        <div class='b'>
+            <img src='img/bg_02.jpg'>
+        </div>
 
-            <a name='contacts'></a>
-            <div class='row  block13 cont'>
-                <div class='contacts'>
-                    +8 926 625 75 90
-                    <br/>
-                    <a href="mailto:info@baker-maker.ru">info@baker-maker.ru</a>
-                    <br/>
-                    <a href="http://www.facebook.com/bakermakermoscow" target="_blank">Facebook</a> и <a href="http://instagram.com/baker_maker/" target="_blank">Instagram</a>
-                </div>
+-->
+
+        <div class='navigation' id='js-navigation'>
+            <ul class='navigation'>
+                <li data-sect='1'></li>
+                <li data-sect='2'></li>
+                <li data-sect='3'></li>
+                <li data-sect='4'></li>
+                <li data-sect='5'></li>
+                <li data-sect='6'></li>
+                <li data-sect='7'></li>
+                <li data-sect='8'></li>
+                <li data-sect='9'></li>
+            </ul>
+        </div>
+
+        <div class='row  block1' id='js-sect1'>
+            <ul class='menu'>
+                <li><a href='#contacts'>Контакты</a></li>
+            </ul>
+        </div>
+        <div class='row  block2'></div>
+        <div class='row  block3'></div>
+
+        <div class='row  block4' id='js-sect2'></div>
+        <div class='row  block5' id='js-sect3'></div>
+        <div class='row  block6' id='js-sect4'></div>
+        <div class='row  block7'></div>
+        <div class='row  block8' id='js-sect5'></div>
+        <div class='row  block9' id='js-sect6'></div>
+        <div class='row  block10' id='js-sect7'></div>
+        <div class='row  block11' id='js-sect8'></div>
+        <div class='row  block12'></div>
+        <div class='row  block13 cont' id='js-sect9'>
+            <a name='contacts' class='anchor'></a>
+            <div class='contacts'>
+                +8 926 625 75 90
+                <br/>
+                <a href="mailto:info@baker-maker.ru">info@baker-maker.ru</a>
+                <br/>
+                <a href="http://www.facebook.com/bakermakermoscow" target="_blank">Facebook</a> и <a href="http://instagram.com/baker_maker/" target="_blank">Instagram</a>
             </div>
         </div>
     </div>
